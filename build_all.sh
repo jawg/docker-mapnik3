944874d023634111c924f89330846289cd119873
Joxit <joxit972@gmail.com>
2018-12-04 19:19:45 +0100

    Add mapnik v3.0.21 add fix builds (use debian stretch)
    Fixes #6

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-for i in {0..19}; do
+for i in {0..21}; do
   echo "Building jawg/mapnik3:3.0.$i"
   docker build --pull -q -t jawg/mapnik3:3.0.$i v3.0.$i
   echo "Pushing jawg/mapnik3:3.0.$i"
